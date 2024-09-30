f7c247ca4caf048da2e0f2410a3e2ec2eb86e6c6
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-09-30 14:50:05 +0200

    add base prompt

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('ciao');
+
+
+let km;
+let age;
+const price_for_km = 0.21;
+let price = km * price_for_km;
+
+km = prompt('Inserisci la distanza da percorrere');
+age = prompt('inserisci la tua età')
+
+console.log(km);
+console.log(age);
