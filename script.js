6e17aad9062d917d1666871f4dcedfeb0cf55904
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-09-30 15:40:26 +0200

    add function for price calc

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ console.log('ciao');
 
 // calcolare il prezzo del biglietto 
 
+function calc_price(km, age) {
+    
+    const price_for_km = 0.21;
+    let price = km * price_for_km;
+
+    // applicare gli sconti
+    
+    if (age < 18) {
+        
+        price = price * 0.8;
+
+    } else if (age >= 65){
+        
+        price = price * 0.6;
+        
+    }
+}
 
 // emissione del prezzo in forma umana
 
